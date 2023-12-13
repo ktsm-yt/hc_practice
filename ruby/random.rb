3d745f55e4a8199ddfc0dfec8c37d9b3472b9005
Yuta Katsuma <ktsm@mba.local>
2023-12-13 20:45:16 +0900

    sampleメソッドに引数を入れて簡略

--- ruby/random.rb
+++ ruby/random.rb
@@ -7,14 +7,15 @@ shuffled_members = members.shuffle
 #グループを3人と3人、または2人と4人にランダムに分ける。
 groups = [[3,3],[2,4]]
 group = groups.sample
-
 #ここまでで[3,3][2,4]のどちらかの配列になる。
-#分けた配列にメンバーを割り当てる。sliceは"配列"から特定の要素を取り出すメソッド
-group1 = shuffled_members.slice(0,group[0])
-group2 = shuffled_members.slice(group[0],group[1])
+
+#sampleメソッドに引数を当てる。
+group1 = shuffled_members.sample(group[0])
+group2 = shuffled_members - group1 #4.7.4 配列の差集合
 
 #表示結果はアルファベット順
+
 p group1.sort
 p group2.sort
 
-# group2 = group - group1 #4.7.4 配列の差集合
+
