ac9db8cb0d3b1251e803d8c544fd90cc82c72f54
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-04-16 01:03:38 +0900

    feat バリデーション構築

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,34 +1,46 @@
 ##ゴルフスコア判定
-#18ホール72合計の条件はどこかに盛り込む?
 
 
 puts ',で区切って既定値を入力してください'
 input_X = gets.chomp
-# Xのバリデーション。int 3 =< X =< 5でなければエラーを出す
 # 配列に格納 文字列を,で分割し、それぞれint化して新配列に
 arr_X = input_X.split(',').map(&:to_i)
-#入れ物はここで作ればおk
+# Xのバリデーション。int 3 =< X =< 5でなければエラーを出す
+if arr_X.any? { |x| x < 3 || x > 5 } #any?は一つでもあるかを問う
+  puts '正しい数字を入力してください'
+  exit
+end
 
 puts ',で区切ってプレイヤーの打数を入力してください'
 input_Y = gets.chomp
+arr_Y = input_Y.split(',').map(&:to_i) 
 # Yのバリデーション。int 1=<でなければエラーを出す
-# 配列に格納
-arr_Y = input_Y.split(',').map(&:to_i)
+if arr_Y.any? { |y| y < 1 } 
+  puts '正しい数字を入力してください'
+  exit
+end
 
-#先に数字にするか、配列のまま計算しちゃうか
-#例外を考えてからにしよう
+valid_length = arr_X.length == 18 && arr_Y.length == 18
+valid_sum = arr_X.sum == 72 && arr_Y.sum == 72
 
+#unlessは1行のときに使うのが通例
+if !valid_length 
+  puts '正しいホール数か確かめてください' 
+  exit
+end
 
-#配列と繰り返しを活用する
-#計算は数値でやってアウトプットの直前で文字列に切り替える
+if !valid_sum
+  puts '正しい規定打数か確かめてください'
+  exit
+end
 
 #zip.mapで配列ごと数値計算ができる
 def calculate(arr_X,arr_Y)
   score_arr = arr_Y.zip(arr_X).map do |y,x|
-      if y == 1 
-        'ホールインワン'
-      elsif x == 5 && y == 1
+      if  x == 5 && y == 1 #条件式の順番修正
         'コンドル'
+      elsif y == 1
+        'ホールインワン'
       elsif x == 5 && y == 2
         'アルバトロス'
       elsif y - x == -2
@@ -47,7 +59,7 @@ def calculate(arr_X,arr_Y)
         "#{y - x}ボギー"
       end
   end
-  #この処理する前にX==5,Y==2,1の処理しないとだね。
+  
   return score_arr.join(',')
   #差し引きを計算した新しい配列
   #出力形式は横の文字列で ,でくくるjoin(',') 
@@ -56,23 +68,21 @@ end
 puts 'スコアを出力します'
 print calculate(arr_X, arr_Y)
 
-# #規定打数より少ない
-# Y - X == 0 'パー'
-# Y - X == -1'バーディ'
-# Y - X == -2'イーグル'
-# Y - X == -3'アルバトロス' #例外あり X == 5 && Y == 2
-# Y - X == -4'コンドル' #例外あり X == 5 && Y == 1
-# Y - X == -5'ホールインワン' Y == 1
-# #規定打数より多い
-# Y - X == 1'ボギー'
-# Y - X == 2'2ボギー'
-# Y - X == 3'3ボギー'
-
-#例外考えたら一括でやるほうが良さそ
 
 
 
-#テストケースを読み込む
-File.open('case_1.txt', 'r')
-#パイプでつなげる?
-cat case_1.txt | ruby golf_score.rb
+# #テストケースを読み込む
+# cat case_1.txt | ruby golf_score.rb
+# これはターミナル操作なので記述するコードではない
+
+# #catでファイルの中身を標準出力してパイプでつなげ  txt→ターミナル
+# #rubyプログラムは標準出力をうけとる  ターミナル→.rb
+
+# ファイル操作のヒント
+# line = File.readlines("case_1.txt")[^0]
+
+# input_line = gets.to_i
+# input_line.times do
+#   s = gets.chomp.split(" ")
+#   print "hello = #{ s[0] } , world = #{ s[1] }\n"
+# end
