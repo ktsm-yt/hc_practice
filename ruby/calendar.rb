6f0942e540cd887bc06a0b7e3412b3f20d350199
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-04-15 14:17:33 +0900

    fix メソッド化と誤字修正

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -8,44 +8,46 @@
 # ruby calendar.rb -m 22
 # 22 is neither a month number (1..12) nor a name"
 
-# ・前提
-# dateのインポート
+# 前提
 require 'date'
-# octparseのインポート
 require 'optparse'
 
-# ・コード
-# 当月・年の表示
-today = Date.today
-puts "      " + "#{today.month}月 #{today.year}"
-#一桁月の0表記を除くコードは煩雑
-
-# 月曜始まりで羅列
-puts "月 火 水 木 金 土 日"
-
-# 最初の日、最後の日を取得
-firstday = Date.new(today.year, today.month, 1)
-lastday = Date.new(today.year, today.month, -1)
-
-# 最初の日の曜日を取得
-weekday = firstday.wday + 1
-print "  " * weekday + " "
-
-# 月初から月末までの羅列
-(firstday..lastday).each do |day|
-  # 日付を右詰めするため、文字列に変換してrjust
-  print day.day.to_s.rjust(2," ") + " "
-  # 7つ目(日曜)で区切る
-    if day.wday == 0
-    puts 
-  end
-end
-
-
-# ・装飾
-# 当日の文字色・背景を反転(応用))
-# print "\e[30today\e[0m"
-# print "\e[47today\e[0m"
 
 # ・オプション指定
 # -mでオプションを出す
+
+
+
+# コード
+
+def calendar
+  # 当月・年の表示
+  today = Date.today
+  puts "      " + "#{today.month}月 #{today.year}"
+  #標準ライブラリには該当なし。一桁月の0表記を除くコードは煩雑
+
+  # 月曜始まりで羅列
+  puts "月 火 水 木 金 土 日"
+
+  # 最初の日、最後の日を取得(date)
+  first_date = Date.new(today.year, today.month, 1)
+  last_date= Date.new(today.year, today.month, -1)
+
+  # 最初の日の曜日を取得 日曜が0なので+1
+  week_day = first_date.wday + 1
+  print " " * week_day
+
+  # 月初から月末までの羅列
+  (first_date..last_date).each do |date|
+    # 日付を右詰めするため、文字列に変換してrjust
+    print date.day.to_s.rjust(2," ") + " "
+    # 7つ目(日曜)で区切り改行
+    puts if date.wday == 0  
+  end
+end
+
+calendar
+# 装飾(要件外)
+# 当日の文字色・背景を反転(応用))
+# print "\e[30today\e[0m"
+# print "\e[47today\e[0m"
