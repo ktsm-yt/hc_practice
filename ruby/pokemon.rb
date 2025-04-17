140453c8bcd81a583d6bb2010d3ff80b29a44c77
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-04-17 18:13:45 +0900

    feat chap4まで

--- ruby/pokemon.rb
+++ ruby/pokemon.rb
@@ -1,11 +1,10 @@
 class Pokemon
-  attr_reader :name, :type1, :type2, :hp, :mp
-  def initialize
+  attr_reader :name, :type1, :type2, :hp
+  def initialize(name, type1, type2, hp)
     @name = name
-    @type1 = type
-    @type2 = type
+    @type1 = type1
+    @type2 = type2
     @hp = hp
-    @mp = mp
   end
   # rubyに静的型付けはない
   # name.is_a?(String)
@@ -18,12 +17,25 @@ class Pokemon
   end
 end
 
-def main
-  pokemons = Array.new(100) {Pokemon.new}
+class Pikachu < Pokemon
+  attr_reader
 
-  puts pokemons[0].name
-  puts pokemons[9].type1
-  puts pokemons[99].attack
+  def attack
+    super
+    puts "#{@name}の10万ボルト"
+  end
+end
+
+
+def main
+  pika = Pikachu.new('ピカチュウ','でんき','', 100)
+  
+  puts pika.attack
+  
+  # pokemons = Array.new(100) {Pokemon.new}
+  # puts pokemons[0].name
+  # puts pokemons[9].type1
+  # puts pokemons[99].attack
 end
 
 main
