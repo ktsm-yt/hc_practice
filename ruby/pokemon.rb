63ab0d2df129a61ef44371d17510959a6be51c29
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-04-17 17:37:06 +0900

    feat chap2まで

--- ruby/pokemon.rb
+++ ruby/pokemon.rb
@@ -1,1 +1,30 @@
+class Pokemon
+  attr_reader :name, :type1, :type2, :hp, :mp
+  def initialize
+    @name = 'リザードン'
+    @type1 = 'ほのお'
+    @type2 = 'ひこう'
+    @hp = 100
+    @mp = 10
+  end
+  # rubyに静的型付けはない
+  # name.is_a?(String)
+  # hp.is_a?(Integer)
+  # で判定はできる
+  
+
+  def attack
+    puts "#{@name}のこうげき"
+  end
+end
+
+def main
+  pokemons = Array.new(100) {Pokemon.new}
+
+  puts pokemons[0].name
+  puts pokemons[9].type1
+  puts pokemons[99].attack()
+end
+
+main
 
