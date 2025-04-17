ab7f96b3bc4f5d1ed360fb2d0cf7827474963929
ktsm <99321574+ktsm-yt@users.noreply.github.com>
2025-04-17 19:09:24 +0900

    feat chap7まで

--- ruby/pokemon.rb
+++ ruby/pokemon.rb
@@ -1,4 +1,26 @@
+# rubyに静的型付けはない
+# name.is_a?(String)
+# hp.is_a?(Integer)
+# で判定はできる
+
+#rubyではモジュール内に処理を書くのが通例
+module NameService 
+  def change_name(new_name)
+    if new_name == 'うんこ'
+      puts '不適切な名前です'
+      return
+    end
+    @name = new_name #これはincludeしたクラスのインスタンス変数
+  end
+
+  def name
+    @name #同様
+  end
+end
+
 class Pokemon
+  include NameService
+  attr_writer :name
   attr_reader :name, :type1, :type2, :hp
   def initialize(name, type1, type2, hp)
     @name = name
@@ -6,24 +28,28 @@ class Pokemon
     @type2 = type2
     @hp = hp
   end
-  # rubyに静的型付けはない
-  # name.is_a?(String)
-  # hp.is_a?(Integer)
-  # で判定はできる
-  
+  #rubyにおいてインスタンス変数はデフォルトでprivate
+  #アクセサメソッドを必ず通さなければならない
+  #カプセル化を推進するような言語設定になっている
 
   def attack
     puts "#{@name}のこうげき"
   end
+
 end
 
 class Pikachu < Pokemon
-  attr_reader
+  attr_reader :name
 
   def attack
     super
     puts "#{@name}の10万ボルト"
   end
+
+end
+
+class Player
+  include NameService
 end
 
 
@@ -40,3 +66,9 @@ end
 
 main
 
+# 動作テスト
+poke = Pokemon.new('test','test','',10)
+poke.change_name('適切')
+puts poke.name
+poke.change_name('うんこ')
+puts poke.name
